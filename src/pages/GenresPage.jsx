3a95c3adda7d21879462af6415c0b7612812e3e1
Daniel <Daniel_Be_95@msn.com>
2023-04-19 11:12:01 +0900

    genre page is showing data, working on layout for the genre movies

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+// Imported react & stuff
+import { getGenre } from '../services/MovieDB'
+import { useQuery } from 'react-query'
+import { useParams } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
 const GenresPage = () => {
+    const { genreId } = useParams()
+    const { data, isLoading, error, isError } = useQuery(['genre', genreId], () => getGenre(genreId))
+
 	return (
-		<div>GenresPage</div>
+		<Container className='my-2 p-2'>
+            {isLoading && (
+				<p>Loading movies...</p>
+			)}
+
+            {isError && (
+				<p>{error.message}</p>
+			)}
+
+            {data && (
+                <>
+                    <h1>Showing data..</h1>
+                </>
+            )}
+        </Container>
 	)
 }
 
