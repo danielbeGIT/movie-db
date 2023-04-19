eb82826038d76f91251218d22b589069032d4dff
Daniel <Daniel_Be_95@msn.com>
2023-04-19 12:00:41 +0900

    genres page added with pagination

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -1,12 +1,34 @@
 // Imported react & stuff
-import { getGenre } from '../services/MovieDB'
+import { getGenre, getGenres } from '../services/MovieDB'
 import { useQuery } from 'react-query'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+
+// Imported components
+import MovieCard from '../components/MovieCard'
+import Pagination from '../components/Pagination'
 
 const GenresPage = () => {
+	const [genreName, setGenreName] = useState('')
+    const [searchParams, setSearchParams] = useSearchParams({query: '', page: 1})
+    const page = searchParams.get('page')
+    
     const { genreId } = useParams()
-    const { data, isLoading, error, isError } = useQuery(['genre', genreId], () => getGenre(genreId))
+    const { data, isLoading, error, isError, previousPage } = useQuery(['genre', genreId, page], () => getGenre(genreId, page), {
+        keepPreviousData: true
+    })
+
+    const getActiveGenre = async () => {
+        const allGenres = await getGenres()
+        const eachGenre = allGenres.genres.find(index => index.id == genreId)
+        setGenreName(eachGenre.name)
+    }
+
+    useEffect(() => {
+        getGenre(genreId, page)
+        getActiveGenre()
+    }, [page, genreId])
 
 	return (
 		<Container className='my-2 p-2'>
@@ -20,7 +42,11 @@ const GenresPage = () => {
 
             {data && (
                 <>
-                    <h1>Showing data..</h1>
+                    <h1>{genreName}</h1>
+
+                    <MovieCard data={data}/>
+
+                    <Pagination page={page} currentPage={setSearchParams} previousPage={previousPage}/>
                 </>
             )}
         </Container>
